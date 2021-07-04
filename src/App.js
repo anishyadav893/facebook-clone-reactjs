40c1c7601c48e5abec5efff5ec9ce1ab547be853
Anish Yadav <yadavanish893@gmail.com>
2021-07-04 16:21:37 -0700

    basic navbar

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   	// using the BEM naming convention of CSS, it helps when apps
   	// are needed to be scaled up
     <div className="app">
-      <h1>Let's build the Facebook clone!</h1>
 
   	   <Header />
 
--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import StoreIcon from '@material-ui/icons/Store';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import AddIcon from '@material-ui/icons/Add';
+import ForumIcon from '@material-ui/icons/Forum'
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+
+import { Avatar, IconButton } from '@material-ui/core';
 
 import './Header.css';
 
@@ -6,12 +18,50 @@ function Header() {
   return <div className = 'header'>
   		<div className = 'header__left'>
   			<img src="https://img.icons8.com/color/96/000000/facebook-new.png" alt = ''/>
+  			<div className = 'header__input'>
+  				<SearchIcon />
+  				<input type = 'text' />
+  			</div>
   		</div>
 
-  		<div className = 'header__mid'></div>
+  		<div className = 'header__mid'>
+  			<div className = 'header__option'>
+  				<HomeIcon fontSize = 'large'/>
+  			</div>
+  			<div className = 'header__option'>
+  				<FlagIcon fontSize = 'large'/>
+  			</div>
+  			<div className = 'header__option'>
+  				<SubscriptionsIcon fontSize = 'large'/>
+  			</div>
+  			<div className = 'header__option'>
+  				<StoreIcon fontSize = 'large'/>
+  			</div>
+  			<div className = 'header__option'>
+  				<SupervisedUserCircleIcon fontSize = 'large'/>
+  			</div>
+  		</div>
 
-  		<div className = 'header__right'></div>
+  		<div className = 'header__right'>
+  			<div className = 'header__info'>
+  				<Avatar />
+  				<h4>Anish Yadav</h4>
+
+  				<IconButton>
+  					<AddIcon />
+  				</IconButton>
+  				<IconButton>
+  					<ForumIcon />
+  				</IconButton>
+  				<IconButton>
+  					<NotificationsActiveIcon />
+  				</IconButton>
+  				<IconButton>
+  					<ExpandMoreIcon />
+  				</IconButton>
+  			</div>
+  		</div>
   </div>;
 }
 
-export default Header
+export default Header;
